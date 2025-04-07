86b4edbe5db88cce8a5e3bde39e2f9ac60adb27c
cfernandom <cristian_21_97@hotmail.com>
2025-04-07 12:10:59 -0500

    feat(repository): implement UserPrismaRepository for user data operations
    Adds UserPrismaRepository for user data operations
    
    Implements user repository methods for CRUD operations.
    
    Enables retrieval, creation, updating, and deletion of user data via Prisma.
    
    Improves data management and integrates with the domain model.

---
+++ src/infrastructure/db/UserPrismaRepository.ts
@@ -0,0 +1,77 @@
+import { PrismaClient } from '@prisma/client'
+import { IUserRepository } from '../../domain/repositories/IUserRepository'
+import { inject, injectable } from 'tsyringe'
+import { User } from '../../domain/entities/User'
+
+@injectable()
+export class UserPrismaRepository implements IUserRepository {
+  constructor (
+    @inject('PrismaClient') private readonly prisma: PrismaClient
+  ) {}
+
+  private toDomainEntity (dbUser: any): User {
+    return new User({
+      id: dbUser.id,
+      email: dbUser.email,
+      passwordHash: dbUser.password_hash,
+      firstName: dbUser.first_name,
+      lastName: dbUser.last_name,
+      createdAt: dbUser.created_at,
+      updatedAt: dbUser.updated_at
+    })
+  }
+
+  async findByEmail (email: string): Promise<User | null> {
+    const dbUser = await this.prisma.user.findUnique({
+      where: { email }
+    })
+    return (dbUser != null) ? this.toDomainEntity(dbUser) : null
+  }
+
+  async findById (id: string): Promise<User | null> {
+    const dbUser = await this.prisma.user.findUnique({
+      where: { id }
+    })
+    return (dbUser != null) ? this.toDomainEntity(dbUser) : null
+  }
+
+  async save (user: User): Promise<User> {
+    const dbUser = await this.prisma.user.create({
+      data: {
+        id: user.id,
+        email: user.email,
+        first_name: user.firstName,
+        last_name: user.lastName,
+        password_hash: user.passwordHash,
+        created_at: user.createdAt,
+        updated_at: user.updatedAt
+      }
+    })
+    return this.toDomainEntity(dbUser)
+  }
+
+  async update (user: User): Promise<User> {
+    const dbUser = await this.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        email: user.email,
+        first_name: user.firstName,
+        last_name: user.lastName,
+        password_hash: user.passwordHash,
+        updated_at: user.updatedAt
+      }
+    })
+    return this.toDomainEntity(dbUser)
+  }
+
+  async delete (id: string): Promise<void> {
+    await this.prisma.user.delete({
+      where: { id }
+    })
+  }
+
+  async findAll (): Promise<User[]> {
+    const dbUsers = await this.prisma.user.findMany()
+    return dbUsers.map((dbUser) => this.toDomainEntity(dbUser))
+  }
+}
