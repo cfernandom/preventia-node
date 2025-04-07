c63be2e0dab39d977728551261b68d8bc5a5865f
cfernandom <cristian_21_97@hotmail.com>
2025-04-07 12:07:05 -0500

    feat(user): implement User entity with validation schema and factory method
    Implements User entity with validation and factory method
    
    Adds a User class with properties and validation schema using Zod.
    
    Introduces a factory method for creating new users, ensuring data integrity.
    
    Enhances user data handling and validation for better security and reliability.

---
+++ src/domain/entities/User.ts
@@ -0,0 +1,54 @@
+import { z } from 'zod'
+
+export class User {
+  readonly id: string
+  readonly email: string
+  readonly passwordHash: string
+  readonly firstName: string
+  readonly lastName: string
+  readonly createdAt: Date
+  updatedAt?: Date
+
+  constructor (props: UserProps) {
+    UserSchema.parse(props)
+    this.id = props.id
+    this.email = props.email
+    this.passwordHash = props.passwordHash
+    this.firstName = props.firstName
+    this.lastName = props.lastName
+    this.createdAt = props.createdAt
+    this.updatedAt = props.updatedAt
+  }
+
+  static create (
+    email: string,
+    passwordHash: string,
+    firstName: string,
+    lastName: string
+  ): User {
+    const props: UserProps = {
+      id: crypto.randomUUID(),
+      email,
+      passwordHash,
+      firstName,
+      lastName,
+      createdAt: new Date()
+    }
+
+    return new User(props)
+  }
+}
+
+export const UserSchema = z.object({
+  id: z.string().uuid(),
+  email: z.string().email('Invalid email format'),
+  passwordHash: z.string().min(10, 'Password must be at least 10 characters long'),
+  firstName: z.string().min(1, 'First name is required'),
+  lastName: z.string().min(1, 'Last name is required'),
+  createdAt: z.date().default(new Date()),
+  updatedAt: z.date().optional()
+})
+
+export type UserProps = z.infer<typeof UserSchema>
+export type UserUpdateProps = Partial<UserProps>
+export type UserCreateProps = Omit<UserProps, 'id' | 'created_at' | 'updated_at'>
