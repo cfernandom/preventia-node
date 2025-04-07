1eafbf911ab19f9eea7cbf61e56c08f5b47faeef
cfernandom <cristian_21_97@hotmail.com>
2025-04-07 12:15:40 -0500

    feat(app): restructure app initialization and middleware setup
    Restructures app initialization and middleware setup
    
    Implements a modular approach to routing and middleware.
    
    Adds health check, authentication, and user routes.
    Integrates JWT authentication middleware and centralized error handling.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,34 @@
 import express from 'express'
 
+import { container } from 'tsyringe'
+import { JwtService } from './infrastructure/services/JwtService'
+import authRoutes from './interfaces/http/routes/v1/auth.routes'
+import { jwtAuthMiddleware } from './common/middleware/auth.middleware'
+import { errorHandler } from './common/middleware/error.middleware'
+import healthRoutes from './interfaces/http/routes/v1/health.routes'
+import userRoutes from './interfaces/http/routes/v1/user.routes'
+
+// Inicilizacion de express
 export const app = express()
 
+// Resolución de dependencias
+const jwtService = container.resolve(JwtService)
+
+// Middlewares base
 app.use(express.json())
-app.get('/ping', (_, res) => {
-  res.json({ message: 'pong' })
-})
+app.use(express.urlencoded({ extended: true }))
+
+// Health Check
+app.use('/api/v1/health', healthRoutes)
+
+// Rutas públicas
+app.use('/api/v1/auth', authRoutes)
+
+// Middleware de autenticación JWT
+app.use('/api/v1', jwtAuthMiddleware(jwtService))
+
+// Rutas protegidas
+app.use('/api/v1/users', userRoutes)
+
+// Manejo centralizado de errores (DEBE ser el último middleware)
+app.use(errorHandler)
